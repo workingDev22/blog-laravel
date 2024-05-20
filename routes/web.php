76cf07c02f0da803a22ca5c651ce17481b4a62c6
Ramaz <you@example.com>
2024-05-20 16:42:59 +0300

    Added resources js css files for admin

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::group(['namespace' => 'Main'], function () {
    Route::get('/', 'IndexController');
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
 
 Auth::routes();
 
