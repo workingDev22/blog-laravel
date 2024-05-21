98c7a5413012a07388c6aed6ff2817b4e828894b
workingDev22 <workingDev22@gmail.com>
2024-05-21 16:45:05 +0500

    added working link to admin.categories.index

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,7 +5,7 @@
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('admin.categories.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-th-list"></i>
                     <p>
                         Категории
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
         Route::get('/', 'IndexController');
     });
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('admin.categories.index');
     });
 });
 
