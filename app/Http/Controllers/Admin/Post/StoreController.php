d17d02fb72ef771633e01591ce0fec68e89d0dbb
workingdev22 <you@example.com>
2024-05-26 18:39:03 +0300

    move domain logic from StoreController to PostService

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -5,21 +5,15 @@ namespace App\Http\Controllers\Admin\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Models\Post;
+use App\Services\PostService;
 use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
-    public function __invoke(StoreRequest $request)
+    public function __invoke(StoreRequest $request, PostService $service)
     {
         $data = $request->validated();
-        $data['preview_file'] = Storage::disk('public')->put('/images', $data['preview_file']);
-        $data['main_file'] = Storage::disk('public')->put('/images', $data['main_file']);
-
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
-
-        $post = Post::firstOrCreate($data);
-        $post->tags()->attach($tagIds);
+        $service->store($data);
 
         return redirect()->route('admin.post.index');
     }
---
+++ app/Services/PostService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    public function store($data)
+    {
+        try {
+            DB::beginTransaction();
+            if(isset($data['preview_file']))
+                $data['preview_file'] = Storage::disk('public')->put('/images', $data['preview_file']);
+
+            if(isset($data['main_file']))
+                $data['main_file'] = Storage::disk('public')->put('/images', $data['main_file']);
+
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagIds);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            abort(500);
+        }
+    }
+
+
+}
