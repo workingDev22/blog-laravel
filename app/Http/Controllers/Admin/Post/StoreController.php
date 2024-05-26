9fa8d10acc218d374405e62e99ae678da9d1d005
workingDev22 <workingDev22@gmail.com>
2024-05-26 14:10:36 +0300

    changed location for images in post

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -12,16 +12,14 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $data['preview_file'] = Storage::put('/images', $data['preview_file']);
-        $data['main_file'] = Storage::put('/images', $data['main_file']);
+        $data['preview_file'] = Storage::disk('public')->put('/images', $data['preview_file']);
+        $data['main_file'] = Storage::disk('public')->put('/images', $data['main_file']);
 
         $tagIds = $data['tag_ids'];
         unset($data['tag_ids']);
 
         $post = Post::firstOrCreate($data);
         $post->tags()->attach($tagIds);
-
-
 
         return redirect()->route('admin.post.index');
     }
