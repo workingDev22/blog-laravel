9aa02919b0ae74d2ff0841a20fc3bf2b2e8697d3
workingDev22 <workingDev22@gmail.com>
2024-05-26 12:30:59 +0300

    added, connect and init select2 js plugin

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -9,11 +9,15 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
+
+    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
 
@@ -207,6 +211,8 @@
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <!-- AdminLTE App -->
+<script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
+
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <script>
     $(document).ready(function() {
@@ -227,6 +233,9 @@
 
     $(function () {
         bsCustomFileInput.init();
+    });
+
+    $('.select2').select2({
     });
 
 </script>
